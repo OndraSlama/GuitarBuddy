0a3e78703a400d057b5fd70bcd0c740dd43894f7
Gandalf <ondra.slama1@gmail.com>
2021-06-01 10:04:11 +0200

    Removed uuid from requirements

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import Vuex from "vuex";
 import * as firebase from "firebase";
 import normalizeString from "../functions/normalizeText";
-// import { uuid } from 'vue-uuid';
 
 Vue.use(Vuex);
 
